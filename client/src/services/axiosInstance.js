bb31074bce79ad93c562cd05cb11767d5e4a9faf
Jayp33333 <johnpauljamito5@gmail.com>
2025-07-27 11:48:24 +0800

    jp1

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
+const baseURL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:5555'
+    : 'https://zootopia-khvj.onrender.com/'; // ✅ Replace with your actual Render backend URL
+
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
+  baseURL,
+  headers: { 'Content-Type': 'application/json' },
 });
 
 export default axiosInstance;
