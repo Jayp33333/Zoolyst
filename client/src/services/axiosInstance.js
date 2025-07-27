09cf6aee0d888734bc28adade9534e1f17065053
Jayp33333 <johnpauljamito5@gmail.com>
2025-07-27 12:02:36 +0800

    updated

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   window.location.hostname === 'localhost'
     ? 'http://localhost:5555'
-    : 'https://zootopia-khvj.onrender.com/'; // ✅ Replace with your actual Render backend URL
+    : 'https://zoolyst.onrender.com/'; // ✅ Replace with your actual Render backend URL
 
 const axiosInstance = axios.create({
   baseURL,
