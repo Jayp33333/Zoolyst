256a63955c2812a948ea3095122d9393e9caeb66
Jayp33333 <johnpauljamito5@gmail.com>
2025-09-24 11:40:08 +0800

    fixing badge

--- client/src/components/GetBadge.jsx
+++ client/src/components/GetBadge.jsx
@@ -1,33 +1,9 @@
 import React from 'react'
 
 const GetBadge = ({type}) => {
-    if (type === 'Mammal') {
+    if (type === 'Mammal' || type === 'Bird' || type === 'Reptile' || type === 'Amphibian' || type === 'Fish') {
       return (
-        <span className="inline-block px-2.5 py-0.5 text-xs font-medium bg-black/50 text-[#fbff0f] rounded-md border border-[#fbff0f]">
-          {type}
-        </span>
-      );
-    } else if (type === 'Bird') {
-      return (
-        <span className="inline-block px-2.5 py-0.5 text-xs font-medium bg-black/50 text-[#ff08b1] rounded-md border border-[#ff08b1]">
-          {type}
-        </span>
-      );
-    } else if (type === 'Fish') {
-      return (
-        <span className="inline-block px-2.5 py-0.5 text-xs font-medium bg-black/50 text-[#00a6ff] rounded-md border border-[#00a6ff]">
-          {type}
-        </span>
-      );
-    } else if (type === 'Reptile') {
-      return (
-        <span className="inline-block px-2.5 py-0.5 text-xs font-medium bg-black/50 text-[#77ff00] rounded-md border border-[#77ff00]">
-          {type}
-        </span>
-      );
-    } else if (type === 'Amphibian') {
-      return (
-        <span className="inline-block px-2.5 py-0.5 text-xs font-medium bg-black/50 text-[#ff870f] rounded-md border border-[#ff870f]">
+        <span className="inline-block px-2.5 py-0.5 text-xs font-medium backdrop-blur-xl bg-black/20 text-white rounded-md border border-white">
           {type}
         </span>
       );
